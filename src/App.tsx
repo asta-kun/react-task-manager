3adb748155bd8605e4d2e39c0e8ad379d99968f2
Héctor Castillo <hectordev1999@gmail.com>
2021-01-09 23:08:15 -0600

    Feat: Add toast provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import 'normalize.css';
 import './app.css';
 import ModuleRouter from './modules/_router';
 import { TaskManagerContextProvider } from './management/task-manager';
+import { ToastProvider } from 'react-toast-notifications';
 
 const App = (): ReactElement => {
   return (
-    <TaskManagerContextProvider>
-      <ModuleRouter />
-    </TaskManagerContextProvider>
+    <ToastProvider autoDismiss={true} autoDismissTimeout={3000}>
+      <TaskManagerContextProvider>
+        <ModuleRouter />
+      </TaskManagerContextProvider>
+    </ToastProvider>
   );
 };
 
