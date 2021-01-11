20608abc650f786806c2a53fae496bfd9c52cf9a
Héctor Castillo <hectordev1999@gmail.com>
2021-01-10 21:59:01 -0600

    Feat: Countdown animation

--- src/modules/taskManager/components/timer.style.ts
+++ src/modules/taskManager/components/timer.style.ts
@@ -4,15 +4,33 @@ const useStyles = makeStyles(() => ({
   root: {
     padding: '.5em 1em',
     borderRadius: 4,
+  },
+
+  time: {
     background: '#eaeaea',
     color: '#929292',
+    fontSize: '3.5em',
+    overflow: 'hidden',
+    height: '1.2em',
+    textAlign: 'center',
+    position: 'relative',
+    borderRadius: 5,
+    '& span': {
+      zIndex: 5,
+      display: 'block',
+      left: '25%',
+      position: 'absolute',
+    },
     '&.active': {
       color: '#fff',
-      background: '#28acec',
+      background: '#4168d6',
     },
   },
-  time: {
-    fontSize: '3.5em',
+  loading: {
+    height: '100%',
+    background: '#b54d4d',
+    zIndex: 4,
+    position: 'absolute',
   },
 }));
 export default useStyles;
--- src/modules/taskManager/components/timer.tsx
+++ src/modules/taskManager/components/timer.tsx
@@ -1,23 +1,81 @@
 import { Box, Grid, IconButton } from '@material-ui/core';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback, useEffect, useRef } from 'react';
 import useStyles from './timer.style';
 import AddIcon from '@material-ui/icons/Add';
 import { useTaskManager } from '../../../management/task-manager';
+import gsap from 'gsap';
+import clsx from 'clsx';
+import PauseIcon from '@material-ui/icons/Pause';
+import StopIcon from '@material-ui/icons/Stop';
+import { useTaskActions } from '../../../api/tasks/list';
+import { TaskStatus } from '../../../request-type/tasks.d';
+import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 
 const Timer = (): ReactElement => {
   const classes = useStyles();
-  const { create } = useTaskManager();
+  const { runningTaskId, tasks, countdown, create } = useTaskManager();
+  const { update } = useTaskActions();
+  const loadingRef = useRef<HTMLDivElement>(null);
+  const { current: tl } = useRef(gsap.timeline({ paused: true }));
+
+  const handlePause = useCallback(() => {
+    runningTaskId && update(runningTaskId, { status: TaskStatus.paused });
+  }, [runningTaskId, update]);
+
+  const handleStop = useCallback(() => {
+    runningTaskId && update(runningTaskId, { status: TaskStatus.completed });
+  }, [runningTaskId, update]);
+
+  const handleReset = useCallback(() => {
+    runningTaskId && update(runningTaskId, { timeElapsed: 0 });
+  }, [runningTaskId, update]);
+
+  // run animation
+  useEffect(() => {
+    if (!runningTaskId || !tasks[runningTaskId]) {
+      tl.clear()
+        .to(loadingRef.current, {
+          duration: 0.1,
+          width: 0,
+        })
+        .play();
+      return;
+    }
+
+    tl.to(loadingRef.current, {
+      duration: 0.1,
+      width: (tasks[runningTaskId].timeElapsed / tasks[runningTaskId].maxTime) * 100 + '%',
+    }).play();
+  }, [loadingRef, tasks, countdown]);
+
   return (
     <Box className={classes.root}>
-      <Grid container>
-        <Grid item xs={9}>
-          <Box className={classes.time}>
-            <span>00:00:00</span>
+      <Grid container alignItems="center" justify="space-between">
+        <Grid item xs={7}>
+          <Box className={clsx(classes.time, runningTaskId && 'active')}>
+            <div className={classes.loading} ref={loadingRef}></div>
+            <span>{countdown}</span>
           </Box>
         </Grid>
-        <Grid item xs={2}>
-          <Grid container>
-            <Grid item xs={12}>
+
+        <Grid item xs={4}>
+          <Grid container justify="space-evenly" alignItems="center">
+            <Grid item xs={3}>
+              <IconButton onClick={handlePause} disabled={!runningTaskId}>
+                <PauseIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={3}>
+              <IconButton onClick={handleStop} disabled={!runningTaskId}>
+                <StopIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={3}>
+              <IconButton onClick={handleReset} disabled={!runningTaskId}>
+                <RotateLeftIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={3}>
               <IconButton onClick={create}>
                 <AddIcon />
               </IconButton>
