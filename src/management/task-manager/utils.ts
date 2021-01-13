2bdf5044600684de2683e955c0f6aab8900019f5
Héctor Castillo <hectordev1999@gmail.com>
2021-01-12 20:43:24 -0600

    Feat: Add function to generate random number

--- src/management/task-manager/utils.ts
+++ src/management/task-manager/utils.ts
@@ -18,3 +18,9 @@ export const getStatusStrByCode = (status: TaskStatus): string => {
       return 'unknown';
   }
 };
+
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
